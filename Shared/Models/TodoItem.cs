925711c8ac06f83601b85a60ccd1232d9ed4660c
Andrew Ettensohn <andrewettensohn@gmail.com>
2020-10-29 13:43:16 -0400

    Add project files.

---
+++ Shared/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanbanMaster.Shared.Models
+{
+    public class TodoItem
+    {
+        public int ToDoItemId { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
