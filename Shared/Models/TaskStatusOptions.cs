0253a3c581865c91bafd223fb67e18caea7e3d48
Andrew Ettensohn <andrewettensohn@gmail.com>
2020-10-30 10:00:49 -0400

    Use filters on small screen

---
+++ Shared/Models/TaskStatusOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanbanMaster.Shared.Models
+{
+    public static class TaskStatusOptions
+    {
+        public static List<string> List = new List<string> { "New", "Doing", "Done" };
+    }
+}
